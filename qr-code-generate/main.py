ea5117227d7683de7004aa709b633e3f3495a63e
amanmath <amatharuu@outlook.com>
2023-02-02 15:11:05 -0500

    testing out flask api call

--- qr-code-generate/main.py
+++ qr-code-generate/main.py
@@ -1,4 +1,14 @@
 from driverInfo import DriverInfo
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/information/getDriverInfo')
+def retrieveDriverInfo():
+    p1 = DriverInfo("Aman", "Matharu", "llS933923ljfd", "82 Universe Drive", "1997-01-15")
+    return p1.infoBehindQR
+
 
 def main():
     p1 = DriverInfo("Aman", "Matharu", "llS933923ljfd", "82 Universe Drive", "1997-01-15")
